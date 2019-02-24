87956b8e99bc4d158c6627f3e52ed56cedfaab43
MarcosJLR <socram.693@gmail.com>
2019-02-24 15:36:27 -0400

    Date fixed

--- Tarea1/date.h
+++ Tarea1/date.h
@@ -1,5 +1,8 @@
 #ifndef DATE_H
 #define DATE_H
-long dateToInt(int *day, int *month, int *year);
+
+long dateToInt(long day, long month, long year);
+
 void intToDate(long date, int *day, int *month, int *year);
+
 #endif
